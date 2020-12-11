7dab5539e69dcb8a66ad8d06f5be0866a7b083f7
sallytareq <sally.ammous@gmail.com>
2020-12-12 01:57:49 +0200

    fixes

--- data_structures_and_algorithms/challenges/linked_list/linked_list.py
+++ data_structures_and_algorithms/challenges/linked_list/linked_list.py
@@ -25,10 +25,11 @@ class Node:
         self.previous = None
 
 class LinkedList:
-    links = []
+    
     def __init__(self):
         self.head = None
         self.size = 0
+        self.links = []
 
     def insert(self, data):
         node = Node(data)
@@ -39,11 +40,11 @@ class LinkedList:
             node.next = self.head
             self.head = node
 
-        LinkedList.links.insert(0, self.head.value)
+        self.links.insert(0, self.head.value)
         self.size += 1
     
     def includes(self, num):
-        for x in LinkedList.links:
+        for x in self.links:
             if x == num:
                 return True
         else: 
@@ -55,7 +56,7 @@ class LinkedList:
     def __str__(self):
         start = "{ "
         curly = " } -> { "
-        string = curly.join(str(n) for n in LinkedList.links)
+        string = curly.join(str(n) for n in self.links)
         end = " } -> NULL"
         return f"{start}{string}{end}"
 
@@ -73,88 +74,105 @@ class LinkedList:
                 index += 1
             current.next = node
             node.previous = current
+            node.next = None
 
-        LinkedList.links.insert(index, node.value)
+        self.links.insert(index, node.value)
         self.size += 1
 
     def insertBefore(self, val, newVal):
-        node = Node(newVal)
         if self.head.value == val:
-            node.next = self.head
-            self.head = node
-            index = 0
+            self.insert(newVal)
         else:
-            current = self.head
-            index = 1
-            while current.next.value != val:
-                current = current.next
-                index += 1
-            node.previous = current.previous
-            current.previous = node
-            current.next = current
-            current = node
-        LinkedList.links.insert(index, node.value)
-        self.size += 1
+            node = Node(newVal)
+            current = self.head.next
+            index = 0
+            if self.includes(val):
+                while index <= self.size:
+                    if current != val:
+                        # print(current.value)
+                        if current.next != None:
+                            current = current.next
+                            index += 1
+                        else: break
+                node.previous = current.previous
+                current.previous = node
+                current.next = current
+                current = node    
+                self.links.insert(index, node.value)
+                self.size += 1
+            else:
+                return "invalid"            
 
     def insertAfter(self, val, newVal):
-        node = Node(newVal)
         if self.head.value == val:
+            node = Node(newVal)
             node.previous = self.head
             self.head.next = node
             index = 0
         else:
+            node = Node(newVal)
             current = self.head
             index = 1
             while current.value != val:
                 current = current.next
                 index += 1
+                    
             node.previous = current
             node.next = current.next
             current.next = node
-        LinkedList.links.insert(index, node.value)
-        self.size += 1
+
+            self.links.insert(index, node.value)
+            self.size += 1
 
     def kthFromEnd(self,k):
-        if k < 0 or k > len(LinkedList.links):
+        if k < 0 or k > len(self.links):
             return "Invalid Input"
-        if k == 1:
+        if k == len(self.links):
             return self.head.value
         
-        index = len(LinkedList.links)-k
-        counter = 0
+        index = len(self.links)-k
+        counter = 1
         current = self.head
         while counter < index:
-            current = current.next
+            if current.next != None:
+                current = current.next
+            else: break
             counter += 1
         num = current.value
         return num
 
 
 if __name__ == "__main__":
-    LinkedList.links = []
-    lnk = LinkedList()
-    lnk.insert(3)
-    lnk.insert(5)
-    # x = lnk.size
-    # print(x)
-    lnk.insert(8)
-    # y = lnk.size
-    # p = lnk.head.value
-    # n = lnk.head.next.value
-    # print(y , p , n)
-    # t = lnk.includes(5)
-    # f = lnk.includes(3)
-    # print(t , f )
-    s = lnk
-    print(s)
-    lnk.append(7)
-    lnk.append(1)
-    print(s)
-    lnk.insertAfter(5,4)
-    print(s)
-    lnk.insertAfter(1,3)
-    print(s)
-    lnk.insertBefore(7,9)
-    print(s)
-    lnk.insertBefore(8,2)
-    print(s)
+
+    # lnk = LinkedList()
+    # lnk.insert(3)
+    # lnk.insert(2)
+    # lnk.insert(1)
+    # lnk.append(4)
+    # lnk.append(5)
+    # lnk.append(6)
+
+    # print(lnk.kthFromEnd(1))
+    # print(lnk.kthFromEnd(2))
+    # print(lnk.kthFromEnd(3))
+    # print(lnk.kthFromEnd(4))
+    # print(lnk.kthFromEnd(0))
+    # print(lnk.kthFromEnd(-1))
+
+    # before_after = LinkedList()
+    # before_after.links = []
+    # before_after.insert(1)
+    # before_after.append(2)
+    # before_after.append(3)
+    # before_after.append(4)
+    # before_after.append(5)
+    # before_after.append(6)
+    # before_after.append(7)
+    # before_after.append(8)
+    # before_after.append(9)
+    # before_after.insertBefore(1,0)
+    # before_after.insertAfter(1,10)
+    # before_after.insertAfter(5,11)
+    # before_after.insertBefore(7,12)
+    # print(before_after.links)
+    pass
--- tests/challenges/test_ll_zip.py
+++ tests/challenges/test_ll_zip.py
@@ -1,38 +1,91 @@
 from data_structures_and_algorithms.challenges.ll_zip.ll_zip import zipLists
+from data_structures_and_algorithms.challenges.linked_list.linked_list import LinkedList, Node
+import pytest
 
-def test_merged_head():
-    list1 = [1,2,3,4,5]
-    list2 = [6,7,8,9,10]
-    expected = list1[0]
-    actual = zipLists(list1,list2)[0]
+# def test_merged_head():
+#     list1 = [1,2,3,4,5]
+#     list2 = [6,7,8,9,10]
+#     expected = list1[0]
+#     actual = zipLists(list1,list2)[0]
+#     assert expected == actual
+
+# def test_merged_in_list():
+#     a = [1,2,3,4,5]
+#     b = [6,7,8,9,10]
+#     actual = zipLists(a,b)
+#     expected = [1,6,2,7,3,8,4,9,5,10]
+#     assert expected == actual    
+
+# def test_length():
+#     list1 = [1,2,3,4,5]
+#     list2 = [6,7,8,9,10]
+#     actual = len(zipLists(list1,list2))
+#     expected = 10
+#     assert expected == actual
+
+# def test_single_elements():
+#     a = [1,2,3,4,5]
+#     b = [6,7,8,9,10]
+#     expected = [1,6,2,7,3,8,4,9,5,10]
+#     y = 0
+#     for x in range(10):
+#         if x%2 == 0:
+#             actual = a[y]
+#         else:
+#             actual = b[y]
+#             y +=1
+#         assert expected[x] == actual 
+
+def test_merged_head(list1):
+    expected = 1
+    actual = list1.head.value
     assert expected == actual
 
-def test_merged_in_list():
-    a = [1,2,3,4,5]
-    b = [6,7,8,9,10]
-    actual = zipLists(a,b)
-    expected = [1,6,2,7,3,8,4,9,5,10]
-    assert expected == actual    
+def test_merged_in_list(list1):
+    current = list1.head
+    index =  0
+    control = [1,6,2,7,3,8,4,9,5,10,11]
+    while current != None:
 
-def test_length():
-    list1 = [1,2,3,4,5]
-    list2 = [6,7,8,9,10]
-    actual = len(zipLists(list1,list2))
-    expected = 10
+        actual = current.value
+        expected = control[index]
+        assert expected == actual
+        current = current.next
+        index +=1    
+
+def test_length(list1):
+    current = list1.head
+    counter =  0
+    while current != None:
+        current = current.next
+        counter +=1
+    actual = counter
+    expected = 11
     assert expected == actual
 
-def test_single_elements():
-    a = [1,2,3,4,5]
-    b = [6,7,8,9,10]
-    expected = [1,6,2,7,3,8,4,9,5,10]
-    y = 0
-    for x in range(10):
-        if x%2 == 0:
-            actual = a[y]
-        else:
-            actual = b[y]
-            y +=1
-        assert expected[x] == actual 
+
+@pytest.fixture
+def list1():
+    list1 = LinkedList()
+
+    list1.insert(1)
+    list1.append(2)
+    list1.append(3)
+    list1.append(4)
+    list1.append(5)
+
+    list2 = LinkedList()
+    list2.insert(11)
+    list2.insert(10)
+    list2.insert(9)
+    list2.insert(8)
+    list2.insert(7)
+    list2.insert(6)
+
+    list1 = zipLists(list1,list2)
+
+    return list1
+
 
 
 
